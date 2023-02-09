bf2db250e06871c859ca59a2e61118f89f26fcfb
Hezron Misagal <118042723+hezron99@users.noreply.github.com>
2023-02-09 20:17:55 +0800

    Update server.py

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ app = Flask(__name__,template_folder="templates")
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = '465'
 app.config['MAIL_USERNAME'] = 'hezronmmisagal@gmail.com'
-app.config['MAIL_PASSWORD'] = 'urghnriklqurbihg'
+app.config['MAIL_PASSWORD'] = ''
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
